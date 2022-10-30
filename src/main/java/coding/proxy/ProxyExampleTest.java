9c4c778669b334b6753dc515f888440bd3bbfd9e
mbp_xiaolh@163.com <mbp_xiaolh@163.com>
2022-10-30 22:47:11 +0800

    first commit

---
+++ src/main/java/coding/proxy/ProxyExampleTest.java
@@ -0,0 +1,18 @@
+package coding.proxy;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ProxyExampleTest {
+
+
+    @Test
+    public void test_proxy() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException, InterruptedException {
+        IOrder order = Aspect.getProxy(Order.class, "coding.proxy.TimeUsageAspect");
+        order.pay();
+        order.show();
+    }
+
+
+}
