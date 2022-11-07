f4676735b7b02a9dfabf36a660360eaa116aae52
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-11-07 14:03:40 +0800

    update RandomStringGenerator
    (#2)  1.添加程序说明

--- src/main/java/coding/collection/RandomStringGenerator.java
+++ src/main/java/coding/collection/RandomStringGenerator.java
@@ -2,6 +2,9 @@ package coding.collection;
 
 import java.util.*;
 
+/**
+ * 随机序列生成器
+ */
 public class RandomStringGenerator<T> implements Iterable<T> {
 
     private final List<T> list;
@@ -27,6 +30,7 @@ public class RandomStringGenerator<T> implements Iterable<T> {
 
             @Override
             public T next() {
+                //从List<T> list 选一个词语
                 return list.get((int) (list.size() * Math.random()));
             }
         };
@@ -35,12 +39,14 @@ public class RandomStringGenerator<T> implements Iterable<T> {
 
     public static void main(String[] argv) {
         var list = Arrays.asList("List", "Tree", "Array");
+        //构造一个词语产生器
         var gen = new RandomStringGenerator<String>(list);
 
 //        for(var s: gen) {
 //            System.out.println(s);
 //        }
 
+        //获取迭代器（操作迭代器）
 //        var it = gen.iterator();
 //        for(int i = 0; i < 100; i++) {
 //            System.out.println(it.next());
