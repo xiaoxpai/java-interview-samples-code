a13db7c994af9f6bb40d801ec3ea68e4ba0ad798
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-11-08 20:16:26 +0800

    Update Aspect.java
    (#2) 给Aspect类添加注释
    2022年11月8日

--- src/main/java/coding/proxy/Aspect.java
+++ src/main/java/coding/proxy/Aspect.java
@@ -13,23 +13,33 @@ public interface Aspect {
     void before();
     void after();
 
+    /**
+     * static : 像静态方法调用一样，
+     * @Param Class<T> <T>：不知道传的是什么类型的，
+     * @Param String ...  可变参数
+     * 
+     * 
+     * 
+     */
     static <T> T getProxy(Class<T> cls, String ... aspects) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
 
         var aspectInsts = Arrays.stream(aspects).map(name -> Try.ofFailable(() -> {
+            //查找类的实例
             var clazz = Class.forName(name);
             return (Aspect)clazz.getConstructor().newInstance();
         }))
-                .filter(aspect -> aspect.isSuccess())
-                .collect(Collectors.toList());
-
+                .filter(aspect -> aspect.isSuccess()) //是否成功拿到了类的实例
+                .collect(Collectors.toList());//收集起来类的实例
+        
+        //上面获取到了类的实例，这里创建实例
         var inst = cls.getConstructor().newInstance();
         return (T) Proxy.newProxyInstance(
                 cls.getClassLoader(),
                 cls.getInterfaces(),
                 new InvocationHandler() {
                     @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable { //method 接口的方法名 args 接口函数的参数
                         for(var aspect : aspectInsts) {
                             aspect.get().before();
                         }
