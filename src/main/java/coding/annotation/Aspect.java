d226bc0237c4878a3709c6635c3d36f3f04ae2be
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-11-09 08:51:50 +0800

    Update Aspect.java
    （#2）给Aspect类添加注释
    2022年11月9日

--- src/main/java/coding/annotation/Aspect.java
+++ src/main/java/coding/annotation/Aspect.java
@@ -4,9 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+//@Deprecated 代表用户不要去用它了
+@Retention(RetentionPolicy.RUNTIME) //注解代表存活时间(编译，运行时，只在源代码中)
+@Target(ElementType.TYPE) //这个注解表示可以用于什么类型上：类型本身
 public @interface Aspect {
     public Class type();
 }
