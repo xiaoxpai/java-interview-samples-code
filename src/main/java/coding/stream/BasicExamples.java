fa16e3cea726b3f687525ec1cd59fddd5cf8523b
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-11-09 11:01:50 +0800

    Update BasicExamples.java
    (#2 ) 给stream流程序添加注释

--- src/main/java/coding/stream/BasicExamples.java
+++ src/main/java/coding/stream/BasicExamples.java
@@ -28,6 +28,23 @@ public class BasicExamples {
         // lambda expression
     }
 
+    
+   /**
+     *   <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+     *      Stream<T> filter(Predicate<? super T> predicate);
+     *      Optional<T> reduce(BinaryOperator<T> accumulator);
+     *           - 把BinaryOperator 把lambda表达式 --> interface 用的
+     *           -   R apply(T t, U u); //两个一样的类型 T、U 产生一个一样类型的结果R
+     *        @HotSpotIntrinsicCandidate
+     *        public static int max(int a, int b) { //max
+     *             return (a >= b) ? a : b;
+     *        }
+     *
+     *            提供流计算：
+     *             有状态： sorted(把乱序，顺序输出)、skip、limit、任何触发状态变化的程序
+     *             无状态： map（一个值映射成另一个值，）、reduce
+     *             副作用：纯函数(pure function)、非纯函数;多写纯函数
+     */
     @Test
     public void test_mapfilterreduce(){
         //var result = Stream.of(1,2,3,4,5,)
@@ -47,11 +64,21 @@ public class BasicExamples {
         stream.forEach(System.out::println);
     }
 
+  /**
+     * 字符串按照字母去重，留下字母，myine
+     *
+     *
+     * <R> Stream<R> flatMap(Function<? super T, ? extends Stream<? extends R>> mapper);
+     *  -  Function<? super T  入参
+     *  -  ? extends Stream<? extends R>  出参
+     *  可以看出是一个   T    -> Stream<R> 的一个映射
+     *  也就是        String -> Stream<R> 的一个映射
+     */
     @Test
     public void test_flatMap(){
         // String -> Stream<R>
         var set = Stream.of("My", "Mine")
-                .flatMap(str -> str.chars().mapToObj(i -> (char)i))
+                .flatMap(str -> str.chars().mapToObj(i -> (char)i)) //int流->mapToObj流->转字符流
                 .collect(Collectors.toSet());
         System.out.println(new ArrayList<>(set));
 //        System.out.println(set.stream().collect(Collectors.toList()));
